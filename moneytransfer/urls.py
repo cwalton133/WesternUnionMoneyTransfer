2cdf9a0f72c2afca2a92832ffd1652dc690102a6
Charles Walton <pythontutorials36@gmail.com>
2025-02-08 10:40:22 +0100

    App urls.py modify for proper navigation

--- moneytransfer/urls.py
+++ moneytransfer/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('forget-password/', views.forget_password, name='forget_password'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('send-money/', views.send_money, name='send_money'),
     path('receive-money/', views.receive_money, name='receive_money'),
